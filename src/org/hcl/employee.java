72b4b16a595c587a3b5f1f52e3917575d398488e
soniyavasan <officialsoniya89@gmail.com>
2023-06-06 20:20:56 +0530

    task

---
+++ src/org/hcl/employee.java
@@ -0,0 +1,31 @@
+package org.hcl;
+
+public class employee {
+	
+	public void empName() {
+
+System.out.println("Employee name = Soniya");
+	}
+	
+
+     public void empId() {
+    	 
+    	 System.out.println("Employee Id = 74586");
+    	 
+     }
+    	 
+    	 public static void main(String[] args) {
+    		 employee e=new employee();
+    		 e.empId();
+    		 e.empName();
+			
+		}
+    	 
+    	
+    	 
+    	 
+}
+
+
+
+
